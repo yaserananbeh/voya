032fe480d8a600d1b651ff3655b03ee9a04c71d3
Yaser Alananbeh <yaser.ananbeh74@gmail.com>
2025-11-23 09:23:10 +0300

    fix editor config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,47 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: [
+          './tsconfig.json',
+          './tsconfig.app.json',
+          './tsconfig.node.json',
+        ],
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'react-hooks': reactHooks,
+    },
+
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked, // ✅ FULL TYPE CHECK
+      prettier, // ✅ no formatting conflict
+    ],
+
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 ])
