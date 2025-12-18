a43046dffc1bd93e6a001e63cdcf9fbd5cba66d6
Yaser Alananbeh <yaser.ananbeh74@gmail.com>
2025-12-18 03:23:48 +0300

    feat(hooks): implement core application hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -5,5 +5,10 @@ export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 export { useNotification } from './useNotification'
-
 export { useRTL } from './useRTL'
+export { useLocalStorage } from './useLocalStorage'
+export { useDebounce } from './useDebounce'
+export { useIntersectionObserver } from './useIntersectionObserver'
+export { useAuth } from './useAuth'
+export { useSearch } from './useSearch'
+export { useBooking } from './useBooking'
