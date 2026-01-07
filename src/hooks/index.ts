aa83dbf490b3c086f031edd620fb1e9a4b5f41e9
Yaser Alananbeh <yaser.ananbeh74@gmail.com>
2026-01-07 08:47:55 +0300

    refactor(hooks): extract Redux hooks to useRedux.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,4 @@
-import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import type { RootState, AppDispatch } from '@/store'
-
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-
+export { useAppDispatch, useAppSelector } from './useRedux'
 export { useNotification } from './useNotification'
 export { useRTL } from './useRTL'
 export { usePageTitle } from './usePageTitle'
